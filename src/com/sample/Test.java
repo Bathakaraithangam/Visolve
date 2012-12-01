59ec0c2da07f2fabb6b56cde5cf3e7d29508cca7
Bathakarai <bathakarai@visolve.com>
2012-12-01 12:58:37 +0530

    NewBranch-commit

--- src/com/sample/Test.java
+++ src/com/sample/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String arg[]) {
 		System.out.println("Test class.....");
 		System.out.println("Updated........");
+		System.out.println("checking.....");
 	}
 }
